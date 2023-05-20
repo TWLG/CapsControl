6a8b66fb69570ff8160a72a4ca669f6415da4029
TWLG <deathweapon@cloisterschool.com>
2023-05-20 03:47:38 -0400

    Create CapsControl.py

---
+++ CapsControl.py
@@ -0,0 +1,14 @@
+# CapsControl quick clipboard text manipulator.
+
+import customtkinter as ctk
+import tkinter as tk
+    
+root = tk.Tk()
+label = tk.Label(root, )
+label.pack()
+
+def main():
+    print("Hello World!")
+main()
+
+# end of file
