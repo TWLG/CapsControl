e19cb49736de06b9a1cf146f9adbf8577e6c1c93
TWLG <deathweapon@cloisterschool.com>
2023-05-21 14:28:13 -0400

    main file
    testing and basic program meat

--- CapsControl.py
+++ CapsControl.py
@@ -1,14 +1,76 @@
 # CapsControl quick clipboard text manipulator.
+import tkinter
+import pyperclip
 
-import customtkinter as ctk
-import tkinter as tk
-    
-root = tk.Tk()
-label = tk.Label(root, )
-label.pack()
+window = tkinter.Tk()
+window.geometry("600x400")
+window.resizable(False, False)
+window.title("CapsControl")
 
+
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=2)
+
+clipboard_text = tkinter.Text(window, height=20, width=40)
+def convertUpper():
+    s = pyperclip.paste()
+    pyperclip.copy(s.upper())
+    print(s.upper())
+    clipboard_text.delete("1.0", "end")
+    clipboard_text.insert(tkinter.END, s)
+
+def convertLower():
+    s = pyperclip.paste()
+    pyperclip.copy(s.lower())
+    print(s.lower())
+    clipboard_text.delete("1.0", "end")
+    clipboard_text.insert(tkinter.END, s)
+
+upper_button = tkinter.Button(
+    window,
+    text="upper",
+    command=convertUpper
+)
+
+upper_button.grid(
+    row=0,
+    column=1
+)
+
+lower_button = tkinter.Button(
+    window,
+    text="lower",
+    command=convertLower
+)
+
+lower_button.grid(
+    row=0,
+    column=2
+)
+
+
+clipboard_text.grid(
+    columnspan=6,
+    row=1,
+    column=1
+)
+
+
+exit_button = tkinter.Button(
+    window,
+    text='Exit',
+    command=lambda: window.quit()
+)
+exit_button.grid(
+    row=0,
+    column=3
+)
 def main():
-    print("Hello World!")
+    window.mainloop()
+
+    convertUpper()
+
+
 main()
 
 # end of file
